a2ac768470353be27d7f3622216253401720a2bb
Mr.Rabbit <Mr.Rabbit@HIEUTTSE61653>
2019-02-20 15:07:43 +0700

    1

---
+++ Test Suites/Agency.ts
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Agency</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>7ef56091-2c9d-418f-8a0d-7bc61bdf00d2</testSuiteGuid>
+   <testCaseLink>
+      <guid>3f67ee62-fa80-47d7-9020-6caceedd74b5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Agency/28AgencyLogin1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5e6e0ce2-735b-4c44-9f53-f6f9803990b4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Agency/29Login2</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>593bfaef-6465-4866-aa4e-5f044ff40c04</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Agency/30ViewProfileAgency1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3c12c9e2-eda9-4698-8757-585d01a6dc63</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Agency/31UpdateProfileAgency</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>54c48a91-2b34-4518-8c0e-a0a8134f6fd4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Agency/32ViewAllDevice1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d2f997b0-a6b3-45a5-9050-81d814ea76ce</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Agency/33CreateRequest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1d5d1703-7944-4ad8-af19-fa3dd23224ad</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Agency/34CreateDevice</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d8ac1540-2cb1-497d-8d39-f8c8accfaea5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Agency/35UpdateDevice</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4581d3d6-3cd5-4dd3-bb4e-5416efad05e1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Agency/36GetDeviceDetails1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d8d82ca4-31b9-4ac0-968c-812511457127</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Agency/37GetTicketByRequestID1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fa6a904a-90b0-4e8e-bf0a-8eb4039b7020</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Agency/38GetDeviceByDeviceTypeID1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3ea19650-ad78-4dbc-aa20-51d69c975c95</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Agency/39ServiceITSupport1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0741ae48-7016-4987-80d2-0efd71e25840</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Agency/40ServiceItem1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>855c7d7a-383e-43a4-9ce3-fc1d40dc5dd5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Agency/41DeviceInAgencyServiceGroup1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2c98d3ae-2b2a-4157-8e9a-b06ae8e6a573</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Agency/42Dashboard1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fdb4a35d-05f2-4b66-9573-b759506fb314</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Agency/43FindITSupporterByRequestID</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f5b173ec-6f6c-47a8-8761-ec0be31cbdac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Agency/44GetDeviceTypeByServiceID</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Company.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Company</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>3e24fe51-d0bc-415c-883a-cecfc2f49e5a</testSuiteGuid>
+   <testCaseLink>
+      <guid>7a16cfc3-a898-40ac-8872-72545dc53ef3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Company/26Company1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
